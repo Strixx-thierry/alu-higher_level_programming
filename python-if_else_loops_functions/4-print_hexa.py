c38b3ea1efe169ced404f63e2d6c80efc0aeef8c
root <t.gusenga@alusstudent>
2024-10-23 08:04:59 -0700

    ok

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if chr(i) not in ('e', 'q'):
-        print("{}".format(chr(i)), end="")
+for i in range(0, 99):
+    print("{0} = 0x{1:x}".format(i, i))
