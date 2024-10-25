fd9225e21963262c289d4d298a9551573478a8be
root <t.gusenga@alusstudent>
2024-10-24 21:33:07 -0700

    ok

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    def find_search(element):
-        return element if element != search else replace
-    return list(map(find_search, my_list))
+def search_replace(list_1, search, replace):
+    return [replace if x == search else x for x in list_1]
