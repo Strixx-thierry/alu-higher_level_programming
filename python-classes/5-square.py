55e81e206d6cbbf12c6e97f16f797119135b9331
root <t.gusenga@alusstudent>
2024-11-05 09:00:08 -0800

    ok

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
 
 
-"""Defines a class Square with a private instance attribute size."""
+"""Square module"""
+
 
 class Square:
+    """ define class"""
 
     def __init__(self, size):
 
-        """init square"""
+        """initalize  square"""
 
         self.size = size
 
     @property
     def size(self):
 
-        """get the current size of square"""
+        """get the current size square decoration"""
         return self.__size
 
     @size.setter
     def size(self, value):
 
-
+        """check for errors """
 
         if type(value) is not int:
             raise TypeError("size must be an integer")
@@ -30,11 +32,12 @@ class Square:
             self.__size = value
 
     def area(self):
+        """ calculate  the area of the square"""
 
         return self.__size ** 2
 
     def my_print(self):
-        """printing function  """
+        """print the funtion of """
 
         for i in range(0, self.__size):
             [print("#", end="") for j in range(self.__size)]
