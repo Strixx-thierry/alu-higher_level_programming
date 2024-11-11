01e92534d8d286528d3eaa2a1f17c5df750b80e4
root <t.gusenga@alusstudent>
2024-11-11 04:09:10 -0800

    ok

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Script that appends command-line arguments to
-a Python list and stores them in a file."""
+"""Script that appends command-line arguments to a
+Python list and stores them in a file"""
 
 from sys import argv
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
@@ -12,9 +12,9 @@ try:
 except FileNotFoundError:
     json_list = []
 
-# Add only the command-line arguments, omitting the script name
+# Append only the arguments, excluding the script name
 for i in argv[1:]:
     json_list.append(i)
 
-# Write the updated list back to the file
+# Save the updated list to the file
 save_to_json_file(json_list, filename)
