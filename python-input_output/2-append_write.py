6c0b22694e0413df621e6145f6ac78eac8867e0f
root <t.gusenga@alusstudent>
2024-11-11 03:54:37 -0800

    ok

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,6 +2,7 @@
 """Create a function that adds a string to the end of a file
 and returns the count of characters appended."""
 
+
 def append_write(filename="", text=""):
     """Function that appends content to a file."""
     with open(filename, "a", encoding="utf-8") as a:
