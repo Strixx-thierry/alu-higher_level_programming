5917183580718968f9b9eb9520039281c64e0141
root <t.gusenga@alusstudent>
2024-11-26 01:14:20 -0800

    ok

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-# Check if a URL is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-size=$(curl -s -o /dev/null -w "%{size_download}" "$1")
-echo "$size"
+# this is a comment
+curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
