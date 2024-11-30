cf9c7b3726fdfd66b7d8a92ca599f373260ad0ac
root <t.gusenga@alusstudent>
2024-11-30 03:34:32 -0800

    ok

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-""" displaying all of the cities"""
+"""
+This script connects to the 'hbtn_0e_0_usa'
+database and lists all the states from the 
+'states' table, displaying each state in the result.
+"""
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
-    """ starts a connection to the database"""
-    with MySQLdb.connect(
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        host='localhost',
-        port=3306
-    )as s:
-        cur = s.cursor()
-        cur.execute(
-                "SELECT * FROM states ORDER BY id ASC"
-            )
-        all_states = cur.fetchall()
-        for each_state in all_states:
-            print(each_state)
-        cur.close()
-        
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states;")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)        
